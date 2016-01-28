c53165fe8ad6d12c2fd95cc0364825db491e7679
stesesia <sesia.stefano@gmail.xom>
2016-01-28 13:45:50 +0000

    Tango with django chapter 5

--- Exercise/views.py
+++ Exercise/views.py
@@ -3,4 +3,13 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("3ch0 says hello to the world")
+
+    # Construct a dictionary to pass to the template engine as its context.
+    # Note the key boldmessage is the same as {{ boldmessage }} in the template!
+    context_dict = {'boldmessage': "Human"}
+
+    # Return a rendered response to send to the client.
+    # We make use of the shortcut function to make our lives easier.
+    # Note that the first parameter is the template we wish to use.
+
+    return render(request, 'Exercise/index.html', context_dict)
