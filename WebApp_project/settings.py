457e494a6e119d24cc526053bca7c0f3c4bb49a1
stesesia <sesia.stefano@gmail.xom>
2016-01-28 13:32:49 +0000

    Tango with django chapter 5

--- WebApp_project/settings.py
+++ WebApp_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+TEMPLATE_PATH = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -23,6 +24,13 @@ SECRET_KEY = ')uc0w*+*^o9)=-i839z0)3nmlwlbh@@(t&i3u7!6c!#!uiprfg'
 DEBUG = True
 
 TEMPLATE_DEBUG = True
+
+TEMPLATE_DIRS = (
+    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
+    # Always use forward slashes, even on Windows.
+    # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
+)
 
 ALLOWED_HOSTS = []
 
